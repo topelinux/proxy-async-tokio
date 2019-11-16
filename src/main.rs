3cc3bb88ac7c7287b877401a5331d77469463b5b
Jie Yang <jie.yang@nanchao.org>
2019-11-16 18:14:14 +0800

    Add init version

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use futures::{future::try_join,TryFutureExt};
+//#![feature(async_await)]
+use tokio::net::{TcpStream, TcpListener};
+use tokio::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8081".parse()?;
+    let mut listener = TcpListener::bind(&addr)?;
+
+    loop {
+
+        if let Ok((client, sock_addr)) = listener.accept().await {
+
+            println!("client from {}", sock_addr);
+            tokio::spawn(async move {
+                let addr = "127.0.0.1:8080".parse().unwrap();
+                let server = TcpStream::connect(&addr).await.unwrap();
+
+                let (mut cr, mut cw) = client.split();
+                let (mut sr, mut sw) = server.split();
+
+                let client_to_server = cr
+                    .copy(&mut sw)
+                    .map(|bytes|{
+                        if let Ok(n) = bytes {
+                            println!("Copyed: {}", n)
+                        }
+                        bytes
+                    })
+                    .map_err(|err| {
+                        println!("err {} for client {}", err, sock_addr);
+                    });
+
+                let server_to_client = sr
+                    .copy(&mut cw)
+                    .map(|bytes|{
+                        if let Ok(n) = bytes {
+                            println!("Copyed: {}", n)
+                        }
+                        bytes
+                    })
+                    .map_err(|err| {
+                        println!("err {} for client {}", err, sock_addr);
+                    });
+                let (_client_to_server, _server_to_client) = try_join(client_to_server, server_to_client).await.unwrap();
+            });
+        }
+    }
+}
